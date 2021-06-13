1e7a6423eae9fb9c19fc302239b2385df97e20e4
NA-Siddiky <siddiky.academic@gmail.com>
2021-06-14 05:08:01 +0600

    fixing looks and features final

--- src/components/Home/Skills/Skills.js
+++ src/components/Home/Skills/Skills.js
@@ -133,98 +133,96 @@ const Skills = () => {
 
 
 
-    const frontEndData = () => {
-        const frontEnd = getSkill.filter((skill) => skill.category === "frontEnd");
-        setGetSkill();
-        console.log(frontEnd);
-    };
-    const backEndData = () => {
-        const backEnd = mySkills.filter((skill) => skill.category === "backEnd");
-        setGetSkill();
+    const frontEndData = mySkills.filter((skill) => skill.category === 'frontEnd');
+    console.log(frontEndData);
 
-    };
-    const toolsData = () => {
-        const tools = mySkills.filter((skill) => skill.category === "tools");
-        setGetSkill();
-    };
+const backEndData = () => {
+    const backEnd = mySkills.filter((skill) => skill.category === "backEnd");
+    setGetSkill();
+
+};
+const toolsData = () => {
+    const tools = mySkills.filter((skill) => skill.category === "tools");
+    setGetSkill();
+};
 
 
-    return (
-        <section className="w-100 container">
+return (
+    <section className="w-100 container">
 
-            <h1 className="text-center section-heading my-5">My Skills</h1>
-            <nav>
-                <div class="nav nav-tabs d-flex justify-content-center align-items-center" id="nav-tab" role="tablist">
-                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Front-End</button>
-                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Back-End</button>
-                    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Tools</button>
-                </div>
-            </nav>
-
-            <div>
-
-                {/* {mySkills.filter((front) => (front.map(() => <p>abcd</p>)))} */}
-
+        <h1 className="text-center section-heading my-5">My Skills</h1>
+        <nav>
+            <div class="nav nav-tabs d-flex justify-content-center align-items-center" id="nav-tab" role="tablist">
+                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Front-End</button>
+                <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Back-End</button>
+                <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Tools</button>
             </div>
-            <div>
+        </nav>
 
-                {/* {mySkills.filter((back) => (back.map(() => <p>abcd</p>)))} */}
+        <div>
 
-            </div>
-            <div>
+            {/* {mySkills.filter((front) => (front.map(() => <p>abcd</p>)))} */}
 
-                {getSkill.map(() => <p>ccc</p>)}
+        </div>
+        <div>
 
-            </div>
+            {/* {mySkills.filter((back) => (back.map(() => <p>abcd</p>)))} */}
+
+        </div>
+        <div>
+
+            {getSkill.map(() => <p>ccc</p>)}
+
+        </div>
 
 
-            <div class="tab-content" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
 
-                    <div className="d-flex justify-content-center align-items-center">
-                        <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
-                            <img src={express} alt="" />
-                        </div>
-                        <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
-                            <img src={tailwindcss} alt="" />
-                        </div>
+                <div className="d-flex justify-content-center align-items-center">
+                    <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
+                        <img src={express} alt="" />
                     </div>
-
-                </div>
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                    <div className="d-flex justify-content-center align-items-center">
-                        <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
-                            <img src={express} alt="" />
-                        </div>
-                        <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
-                            <img src={tailwindcss} alt="" />
-                        </div>
+                    <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
+                        <img src={tailwindcss} alt="" />
                     </div>
                 </div>
-                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                    <div className="d-flex justify-content-center align-items-center">
-                        <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
-                            <img src={express} alt="" />
-                        </div>
-                        <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
-                            <img src={tailwindcss} alt="" />
-                        </div>
+
+            </div>
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                <div className="d-flex justify-content-center align-items-center">
+                    <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
+                        <img src={express} alt="" />
+                    </div>
+                    <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
+                        <img src={tailwindcss} alt="" />
                     </div>
                 </div>
             </div>
-
-
-            {/* Progress Bar  */}
-            <div className="d-flex justify-content-center align-items-center">
-                <div className="w-50">
-                    {
-                        mySkills.map(skill => <SkillDetails done={skill.done} technologyName={skill.technologyName} ></SkillDetails>)
-                    }
+            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                <div className="d-flex justify-content-center align-items-center">
+                    <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
+                        <img src={express} alt="" />
+                    </div>
+                    <div className="skill-item d-flex justify-content-center align-items-center m-4 p-4">
+                        <img src={tailwindcss} alt="" />
+                    </div>
                 </div>
             </div>
-        </section>
+        </div>
 
-    );
+
+        {/* Progress Bar  */}
+        <div className="d-flex justify-content-center align-items-center">
+            <div className="w-50">
+                {
+                    mySkills.map(skill => <SkillDetails done={skill.done} technologyName={skill.technologyName} ></SkillDetails>)
+                }
+            </div>
+        </div>
+    </section>
+
+);
 };
 
 export default Skills;
